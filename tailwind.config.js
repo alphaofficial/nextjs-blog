942cbee64edaf7f3accf29c11a3def69123d4c9c
Albert Alpha <baronofficial@gmail.com>
2021-03-23 12:32:03 +0000

    convert to typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-    purge: [
-      // Use *.tsx if using TypeScript
-      './pages/**/*.js',
-      './components/**/*.js'
-    ]
-    // ...
-  }
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+}
