fca51f4aceca3541f6eadc22241be09e24824aab
Albert Alpha <baronofficial@gmail.com>
2021-03-24 04:42:53 +0000

    update intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,11 +20,7 @@ export default function Home({allPostsData}: {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Albert. I'm a software engineer and an aspiring machine learning engineer. You can contact me on Twitter</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Hello, I'm Albert. I'm a software engineer and an aspiring machine learning engineer</p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
